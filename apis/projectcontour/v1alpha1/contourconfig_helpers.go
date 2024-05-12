84974f63604d709f91226714d6543b6bff2c22d7
Sam <saman2000hoseini@gmail.com>
2024-05-13 02:46:49 +0330

    chore: sync snappcloud with main repo

--- apis/projectcontour/v1alpha1/contourconfig_helpers.go
+++ apis/projectcontour/v1alpha1/contourconfig_helpers.go
@@ -90,6 +90,15 @@ func (t *TracingConfig) Validate() error {
 		}
 		customTagNames = append(customTagNames, customTag.TagName)
 	}
+
+	if t.System != nil {
+		switch *t.System {
+		case TracingSystemOpenTelemetry, TracingSystemZipkin:
+		default:
+			return fmt.Errorf("invalid tracing system %q", *t.System)
+		}
+	}
+
 	return nil
 }
 
