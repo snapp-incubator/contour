3c8f141673b864185874482b25214efff9e69eab
Sam <saman2000hoseini@gmail.com>
2024-05-14 15:11:19 +0330

    fix: correct some issues

--- internal/envoy/v3/accesslog.go
+++ internal/envoy/v3/accesslog.go
@@ -52,8 +52,7 @@ func FileAccessLogEnvoy(path, format string, extensions []string, level contour_
 						},
 					},
 				},
-				OmitEmptyValues: true,
-				Formatters:      extensionConfig(extensions),
+				Formatters: extensionConfig(extensions),
 			},
 		}
 	}
